9d82da0319724e5464b8a8450903b9458d98852b
Edwyn McMillan <codemcmillan@gmail.com>
2021-08-21 13:19:21 +1200

    leap passes tests

--- leap/leap.cpp
+++ leap/leap.cpp
@@ -1,5 +1,10 @@
 #include "leap.h"
 
 namespace leap {
-
-}  // namespace leap
+	bool leap::is_leap_year(int year) {
+		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) 
+			return true;
+		else 
+			return false;
+	}
+}  
