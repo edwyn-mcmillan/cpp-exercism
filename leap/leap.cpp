91225b708ae3113e6decbfdb3517f1f9d5b72f41
Edwyn McMillan <codemcmillan@gmail.com>
2021-08-22 14:52:46 +1200

    leap refactor

--- leap/leap.cpp
+++ leap/leap.cpp
@@ -1,10 +1,7 @@
 #include "leap.h"
 
 namespace leap {
-	bool leap::is_leap_year(int year) {
-		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) 
-			return true;
-		else 
-			return false;
+	bool is_leap_year(int year) {
+		return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
 	}
 }  
