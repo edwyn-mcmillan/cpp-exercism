9563c6bf53fb37f45e9c290ab8ae5d64f2c56590
Edwyn McMillan <codemcmillan@gmail.com>
2021-08-24 14:38:09 +1200

    spaceage progress

--- space-age/space_age.cpp
+++ space-age/space_age.cpp
@@ -1,5 +1,31 @@
 #include "space_age.h"
+#include <string>
+#include <map>
 
 namespace space_age {
 
-}  // namespace space_age
+	std::map<std::string, double> planet{
+  { "Mercury", 0.2408467 },
+  { "Venus", 0.61519726 },
+  { "Earth", 31557600 },
+  { "Mars", 1.8808158 },
+  { "Jupiter", 11.862615 },
+  { "Saturn", 29.447498 },
+  { "Uranus", 84.016846 },
+  { "Neptune", 164.79132 },
+	};
+
+	class space_age {
+	public:
+		space_age(int input) {
+			int seconds = input;
+		}
+	};
+
+	int orbitalPeriodInEarthYears(int period) {
+		return ((space_age.seconds / planet["Earth"]) / period).toFixed(2);
+	};
+
+
+
+}
--- space-age/space_age.h
+++ space-age/space_age.h
@@ -3,6 +3,13 @@
 
 namespace space_age {
 
+	std::map<std::string, double> planet;
+
+	class space_age {
+		int orbitalPeriodInEarthYears(int period);
+	};
+
+
 }  // namespace space_age
 
 #endif // SPACE_AGE_H
