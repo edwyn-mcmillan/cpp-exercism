6a8b5fda4780322c8249008cb2750d958ffa98e1
Edwyn McMillan <codemcmillan@gmail.com>
2021-08-25 14:21:29 +1200

    nucleotide-count passes all tests

--- nucleotide-count/nucleotide_count.cpp
+++ nucleotide-count/nucleotide_count.cpp
@@ -4,30 +4,24 @@
 #include <stdexcept>
 
 namespace nucleotide_count {
+
 	counter::counter(std::string input) {
-
-	   reference = { {"A", 0}, {"C", 0}, {"G", 0}, {"T", 0} };
-
-	   int n = input.length();
-	   char char_array[n + 1];
-	   strcpy(char_array, input.c_str());
-
-		for (int i = 0; i < input.length(); i++) {
-			char character = input[i];
-			reference[character]++;
+		for (char& nucleotide : input) {
+			if (nucleotide == 'A' || nucleotide == 'C' || nucleotide == 'G' || nucleotide == 'T')
+				this->nucleotide_count_reference[nucleotide]++;
+			else
+				throw std::invalid_argument("not a valid nucleotide.");
 		}
 	}
 
 	std::map<char, int> counter::nucleotide_counts() const {
-		return reference;
+		return this->nucleotide_count_reference;
 	}
 
-	int counter::count(char c) const {
-		if (!reference.count(c)) {
-			throw std::invalid_argument("Invalid  nucleotide");
-		}
-
-		return reference.at(c);
+	int counter::count(char nucleotide) const {
+		if (nucleotide == 'A' || nucleotide == 'C' || nucleotide == 'G' || nucleotide == 'T')
+			return this->nucleotide_count_reference.at(nucleotide);
+		else
+			throw std::invalid_argument("not a valid nucleotide.");
 	}
-
-} 
+}
--- nucleotide-count/nucleotide_count.h
+++ nucleotide-count/nucleotide_count.h
@@ -7,14 +7,12 @@
 namespace nucleotide_count {
 	class counter {
 	private:
-	    std::map<std::string, int>  reference;
-		
+		std::map<char, int> nucleotide_count_reference{ {'A', 0}, {'C', 0}, {'G', 0}, {'T', 0} };
 	public:
-		counter(std::string input);
+		counter(std::string);
+		int count(char) const;
 		std::map<char, int> nucleotide_counts() const;
-		int count(char c) const;
-
 	};
-}  
+}
 
 #endif // NUCLEOTIDE_COUNT_H
